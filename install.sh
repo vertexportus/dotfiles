b0dfa06beee3d4bd1f6fa6bcda4e0471718ef220
Daniel Alves <daniel.alves@portus.tech>
2019-05-12 16:54:45 -0300

    install: added configs symlink creation

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 
 CURRENT_SCRIPT=`realpath $0`
 BASE_PATH=`dirname $CURRENT_SCRIPT`
+$SCRIPTS=$BASE_PATH/scripts
 
-source $BASE_PATH/scripts/fonts.rc
+source $SCRIPTS/fonts.rc
+source $SCRIPTS/configs.rc
 
 # install fonts
+create_config_links
 install_fonts
